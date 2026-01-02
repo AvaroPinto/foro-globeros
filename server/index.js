25e71f8dd1b1ac840993be198080fcaa5a89ea61
AvaroPinto <alvaropintoarevalo@gmail.com>
2026-01-03 00:59:47 +0100

    añadimos la carpeta del server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({ ok: true, message: "API funcionando" });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log("API en puerto " + PORT));
